df77a43ad7827728dd6f42218a1007544a08d478
Rupali0311 <rupalitiwary0311@gmail.com>
2025-06-02 22:32:07 +0530

    Project setup

---
+++ src/main/java/com/learn/pay_buddy_backend/PayBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learn.pay_buddy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayBuddyBackendApplication.class, args);
+	}
+
+}
